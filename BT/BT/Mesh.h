42f41132ec66efae9815d161eab8d37d43f1fa87
Simona Backovska <backovska3@uniba.sk>
2019-02-26 17:02:57 +0100

    Added Mesh and Model classes

---
+++ BT/BT/Mesh.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glad/glad.h> 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Shader.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+using namespace std;
--- BT/BT/Shader.h
+++ BT/BT/Shader.h
@@ -22,3 +22,4 @@ public:
 	void setFloat(const std::string &name, float value) const;
 	void setMat4(const std::string &name, const glm::mat4 &mat) const;
 };
+
